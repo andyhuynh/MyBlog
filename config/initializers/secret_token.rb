70bac1f52e8207b8bbb37acacf658be084d92d5a
Andy Huynh <ahuynh@honolulu.gov>
2011-10-14 14:13:07 -1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myblog::Application.config.secret_token = 'be2a31c417ad03e99bd9d5598900457a8a5a0120cc5c89f99b387f5db9a1c90aeadcefa433cbb70dae01f9a159ad9c869562e50464b9f5d0f1277cc00ec397a7'
